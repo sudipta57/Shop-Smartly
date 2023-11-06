db80ba61803100da9be3afcc35b964eb1865ea3a
sudipta57 <ghoramiswapna32@gmail.com>
2023-11-06 11:41:44 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+ let searchInp =  document.getElementById("search-input")
+
+ function searchError() {
+    alert("Hello sir currently you can't search in our site.. you can simply click on amazon.in below it and will continue your shoping!");
+ }
